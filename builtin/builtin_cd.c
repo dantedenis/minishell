a0acd8b2f8e65d9a90018a0cb79d435f514a6219
dantedenis <76144861+dantedenis@users.noreply.github.com>
2022-02-21 10:45:58 +0300

    Update builtin_cd.c

--- builtin/builtin_cd.c
+++ builtin/builtin_cd.c
@@ -17,7 +17,7 @@ static void	change_dir(t_env **env, char *path, int flag)
 	char	*cwd;
 	
 	cwd = getcwd(NULL, 0);
-	if (!chdir(path))
+	if (cwd && !chdir(path))
 	{
 		if (flag)
 			ft_putendl_fd(path, 1);
@@ -28,7 +28,7 @@ static void	change_dir(t_env **env, char *path, int flag)
 	}
 	else
 	{
-		ft_putstr_fd(" cd: ", 2);
+		ft_putstr_fd("minishell: cd: ", 2);
 		ft_putstr_fd(path, 2);
 		if (access(path, F_OK) == -1)
 			ft_putendl_fd(": no such file or directory", 2);
@@ -44,6 +44,8 @@ int	bin_cd(t_env **env, t_list *cmd)
 {
 	if (!cmd)
 		change_dir(env, get_value_env(*env, "HOME"), 0);
+	else if (!ft_strncmp(cmd->contetnt, ".", 2))
+		change_dir(env, get_value_env(*env, "PWD"), 0);
 	else if (!ft_strncmp(cmd->content, "--", 3))
 		change_dir(env, get_value_env(*env, "HOME"), 0);
 	else if (!ft_strncmp(cmd->content, "-", 2))
