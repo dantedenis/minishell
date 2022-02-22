c2c6cc3ba565467d5ebf72fe2a2fda0f0437eeb3
dantedenis <76144861+dantedenis@users.noreply.github.com>
2022-02-22 09:42:29 +0300

    Update builtin_export.c

--- builtin/builtin_export.c
+++ builtin/builtin_export.c
@@ -12,10 +12,43 @@
 
 #include "minishell.h"
 
+static int	checker(char *key, char *value)
+{
+	int	error;
+	int	i;
+
+	error = 0;
+	i  = 0;
+	if (!ft_isalpha(key[i]) || key[i] != '_')
+		error = 1;
+	if (!ft_strchr(value, '='))
+		error = 1;
+	while(key[++i])
+	{
+		if (!ft_isalpha(key[i]) || !ft_isdigit(key[i]))
+		{
+			error = 1;
+			break ;
+		}
+	}
+	if (error)
+	{
+		ft_putstr_fd("minishell: export: ", 2);
+		ft_putstr_fd(key, 2);
+		ft_putchar_fd('=', 2);
+		ft_putstr_fd(value, 2);
+		ft_putendl_fd(": not a valid identifier", 2);
+		return (1);
+	}
+	return (0);
+}
+
 int	bin_export(t_env **env, char *key, char *value)
 {
 	t_env	*temp;
 
+	if (checker(key, value))
+		return (1);
 	temp = get_env(*env, key);
 	if (!temp)
 	{
